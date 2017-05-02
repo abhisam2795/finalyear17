122c115d1905ce2c60b5ee229d87a02e8f801c9d
mounish4882 <mounishr@gmail.com>
2017-05-03 00:01:17 +0530

    Add files via upload

---
+++ acc.php
@@ -0,0 +1,8 @@
+<?php
+    echo "<form action ='accdetails.php' method = 'post'>
+            Account_no:<input type = 'text' name = 'b'><br>
+            <input type = 'submit' value = 'ENTER'>
+            </form>";
+
+?>
+
