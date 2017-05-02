3efa9bf607151ac0d296555a132755a7b61b7e48
nikkheel21 <manthenaa.nikkheel@gmail.com>
2017-05-03 00:12:33 +0530

    Add files via upload

---
+++ Template.php
@@ -0,0 +1,36 @@
+<-
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="css/Layout.css" rel="stylesheet" type="text/css" />
+<link href="css/Menu.css" rel="stylesheet" type="text/css" />
+<link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+<div id="Holder">
+<div id="NavBar">
+	<nav>
+		<ul>
+			<li><a href ="Template.php">Home</a></li>
+			<li><a href ="T_Login.php">Login</a></li>
+			<li><a href ="T_Register.php">Register</a></li>
+		</ul>
+	</nav>		
+</div>
+<div id="Content">
+	<div id ="PageHeading"></div>
+	<h1>WELCOME!</h1>
+	</div> 
+
+	<div id ="ContentLeft"></div>
+	<h2>ONLINE EXAMINATION SYSTEM</h2>
+	<h2>Register/Login to take a practice exam</h2>	
+	<div id ="ContentRight">
+	
+<div id="Footer"></div>
+</div>
+</body>
+</html>
