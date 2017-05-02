c4f3b780a98bca4f1983791b0874da25230182ca
amardaran95 <amardaran18@gmail.com>
2017-05-03 00:10:07 +0530

    Add files via upload

---
+++ T_Login.php
@@ -0,0 +1,71 @@
+
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="css/Layout.css" rel="stylesheet" type="text/css" />
+<link href="css/Menu.css" rel="stylesheet" type="text/css" />
+<link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+<div id="Holder">
+<div id="NavBar">
+	<nav>
+		<ul>
+			<li><a href ="Template.php">Home</a></li>
+			<li><a href ="T_Login.php">Login</a></li>
+			<li><a href ="T_Register.php">Register</a></li>
+		</ul>
+	</nav>		
+</div>
+<div id="Content">
+	<div id ="PageHeading"></div>
+	<h1>LOGIN!</h1>
+	</div> 
+
+	<div id ="ContentLeft"></div>
+	<h2>ONLINE EXAMINATION</h2>	
+	<div id ="ContentRight">
+	
+	</div>
+<div class="row">
+<div class="col-md-offset-3 col-md-6">
+<form class="form-horizontal">
+  <div class="form-group">
+    <label for="inputEmail3" class="col-sm-2 control-label">Username</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputEmail3" placeholder="Username">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+    </div>
+  </div>
+   <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <div class="checkbox">
+        <label>
+          <input type="checkbox"> Remember me
+        </label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-default">Sign in</button>
+    </div>
+  </div>
+</form>
+</div>	
+</div>	
+
+
+</div>	
+<div id="Footer"></div>
+</div>
+</body>
+</html>
