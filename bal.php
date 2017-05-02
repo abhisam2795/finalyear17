2f61f65cf589923b38b84d52c9b5741cc10c9a0e
mounish4882 <mounishr@gmail.com>
2017-05-02 23:59:25 +0530

    Add files via upload

---
+++ bal.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="css/Layout.css" rel="stylesheet" type="text/css" />
+<link href="css/Menu.css" rel="stylesheet" type="text/css" />
+<link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+<div id="Holder">	
+<div id="NavBar">
+	<nav>
+		<ul>
+                        <li><a href ="select.php">HOME</a></li>
+			<li><a href ="accdetails.php">ACCOUNT_DETAILS</a></li>
+			<li><a href ="balance.php">BALANCE</a></li>
+			<li><a href ="fund.php">TRANSFER FUND</a></li>
+                        <li><a href ="transaction.php">TRANSACTION</a></li>
+                        <li><a href ="index.php">LOG OUT</a></li>
+		</ul>
+	</nav>
+	
+<div id="Footer"></div>
+</div>
+    <body>
+        <h2> Password changed </h2>
+    </body>
+</html>
